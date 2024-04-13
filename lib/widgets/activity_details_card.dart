fa9eaa4e5e7c46e2beca324d8d85072ca17c7f0f
anirudhj0204 <77201585+anirudhj0204@users.noreply.github.com>
2024-04-13 18:16:07 +0530

    15 sec reload

--- lib/widgets/activity_details_card.dart
+++ lib/widgets/activity_details_card.dart
@@ -1,98 +1,133 @@
+import 'package:flutter/material.dart';
 import 'package:fitness_dashboard_ui/data/health_details.dart';
+import 'package:fitness_dashboard_ui/model/health_model.dart';
 import 'package:fitness_dashboard_ui/util/responsive.dart';
 import 'package:fitness_dashboard_ui/widgets/custom_card_widget.dart';
-import 'package:flutter/material.dart';
 
-class ActivityDetailsCard extends StatelessWidget {
-  const ActivityDetailsCard({super.key});
+class ActivityDetailsCard extends StatefulWidget {
+  const ActivityDetailsCard({Key? key}) : super(key: key);
 
   @override
+  _ActivityDetailsCardState createState() => _ActivityDetailsCardState();
+}
+
+class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
+  late Future<List<HealthModel>> _healthDataFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start fetching health data immediately and every 15 seconds
+    _fetchHealthData();
+  }
+
+  void _fetchHealthData() {
+    _healthDataFuture = HealthDetails().getHealthData();
+    Future.delayed(const Duration(seconds: 15), () {
+      if (mounted) {
+        setState(() {
+          _fetchHealthData(); // Fetch again after 15 seconds
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final healthDetails = HealthDetails();
-
-    return GridView.builder(
-      itemCount: healthDetails.healthData.length,
-      shrinkWrap: true,
-      physics: const ScrollPhysics(),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: Responsive.isMobile(context) ? 2 : 4,
-        crossAxisSpacing: Responsive.isMobile(context) ? 12 : 15,
-        mainAxisSpacing: 12.0,
-      ),
-      itemBuilder: (context, index) {
-        if (index == 3) {
-          // Check if it's the fourth card
-          return CustomCard(
-              color: Color.fromARGB(255, 224, 224, 224),
-              child: ElevatedButton(
-                onPressed: () {
-                  // hadle click
-                },
-                style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.all(10),
-                            backgroundColor: Color.fromARGB(255, 224, 224, 224)),
-                
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      healthDetails.healthData[index].icon,
-                      width: 60,
-                      height: 60,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15),
-                      
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      healthDetails.healthData[index].title,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        color: Color.fromARGB(255, 58, 58, 58),
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ],
-                ),
-              ));
+    return FutureBuilder<List<HealthModel>>(
+      future: _healthDataFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
         } else {
-          return CustomCard(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  healthDetails.healthData[index].icon,
-                  width: 50,
-                  height: 50,
-                  colorBlendMode: BlendMode.lighten,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15, bottom: 4),
-                  child: Text(
-                    healthDetails.healthData[index].value,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
+          List<HealthModel>? healthDetails = snapshot.data;
+
+          return GridView.builder(
+            itemCount: healthDetails != null ? healthDetails.length : 0,
+            shrinkWrap: true,
+            physics: const ScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: Responsive.isMobile(context) ? 2 : 4,
+              crossAxisSpacing: Responsive.isMobile(context) ? 12 : 15,
+              mainAxisSpacing: 12.0,
+            ),
+            itemBuilder: (context, index) {
+              if (index == 3) {
+                return CustomCard(
+                  color: const Color.fromARGB(255, 224, 224, 224),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // handle click
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(10),
+                      backgroundColor: const Color.fromARGB(255, 224, 224, 224),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          healthDetails != null ? healthDetails[index].icon : "",
+                          width: 60,
+                          height: 60,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 15),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          healthDetails != null ? healthDetails[index].title : "",
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 58, 58, 58),
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                Text(
-                  healthDetails.healthData[index].title,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.normal,
+                );
+              } else {
+                return CustomCard(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        healthDetails != null ? healthDetails[index].icon : "",
+                        width: 50,
+                        height: 50,
+                        colorBlendMode: BlendMode.lighten,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15, bottom: 4),
+                        child: Text(
+                          healthDetails != null ? healthDetails[index].value.toString() : "", // Convert double to String
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        healthDetails != null ? healthDetails[index].title : "",
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
+                );
+              }
+            },
           );
         }
       },
